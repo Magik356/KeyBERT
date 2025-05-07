37f815f5bd93c6c9cfd314e70c1a3df9dfef244b
Marty Schwartz <165899486+Magik356@users.noreply.github.com>
2025-05-07 13:46:10 -0500

    Update _spacy.py
    Update spacy transformer backend to use the new CuratedTransformer

--- keybert/backend/_spacy.py
+++ keybert/backend/_spacy.py
@@ -1,11 +1,13 @@
 import numpy as np
 from tqdm import tqdm
 from typing import List
+from packaging import version
+from spacy import __version__ as spacy_version
 from keybert.backend import BaseEmbedder
 
 
 class SpacyBackend(BaseEmbedder):
-    """Spacy embedding model.
+    """Spacy embedding model
 
     The Spacy embedding model used for generating document and
     word embeddings.
@@ -54,8 +56,13 @@ class SpacyBackend(BaseEmbedder):
     def __init__(self, embedding_model):
         super().__init__()
 
+        self.curated_transformers = False
+        
         if "spacy" in str(type(embedding_model)):
             self.embedding_model = embedding_model
+            if "transformer" in self.embedding_model.component_names:
+                if version.parse(spacy_version) >= version.parse("3.7.0"):
+                    self.curated_transformers = True   
         else:
             raise ValueError(
                 "Please select a correct Spacy model by either using a string such as 'en_core_web_md' "
@@ -63,7 +70,8 @@ class SpacyBackend(BaseEmbedder):
             )
 
     def embed(self, documents: List[str], verbose: bool = False) -> np.ndarray:
-        """Embed a list of n documents/words into an n-dimensional matrix of embeddings.
+        """Embed a list of n documents/words into an n-dimensional
+        matrix of embeddings
 
         Arguments:
             documents: A list of documents or words to be embedded
@@ -73,14 +81,33 @@ class SpacyBackend(BaseEmbedder):
             Document/words embeddings with shape (n, m) with `n` documents/words
             that each have an embeddings size of `m`
         """
+
         # Extract embeddings from a transformer model
         if "transformer" in self.embedding_model.component_names:
             embeddings = []
             for doc in tqdm(documents, position=0, leave=True, disable=not verbose):
                 try:
-                    embedding = self.embedding_model(doc)._.trf_data.tensors[-1][0].tolist()
-                except:  # noqa: E722
-                    embedding = self.embedding_model("An empty document")._.trf_data.tensors[-1][0].tolist()
+                    if self.curated_transformers:
+                        embedding = (
+                            self.embedding_model(doc)._.trf_data.last_hidden_layer_state.data[0].tolist()
+                        )
+                    else:
+                        embedding = (
+                            self.embedding_model(doc)._.trf_data.tensors[-1][0].tolist()
+                        )
+                except:
+                    if self.curated_transformers:
+                        embedding = (
+                            self.embedding_model("An empty document")
+                            ._.last_hidden_layer_state.data[0]
+                            .tolist()
+                        )
+                    else:
+                        embedding = (
+                            self.embedding_model("An empty document")
+                            ._.trf_data.tensors[-1][0]
+                            .tolist()
+                        )
                 embeddings.append(embedding)
             embeddings = np.array(embeddings)
 
